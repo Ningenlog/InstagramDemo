a3ab3ea85cbab0830a602786b4cbc3cc03fc967a
Ningenlog <a874305231@163.com>
2017-08-10 19:45:30 +0800

    添加了键盘的notification

--- Instagram/SignUpVC.swift
+++ Instagram/SignUpVC.swift
@@ -32,17 +32,32 @@ class SignUpVC: UIViewController {
         scrollView.contentSize.height = self.view.frame.height
         scrollViewHeight = self.view.frame.height
         
-        //
+        //NotificationCenter ...
         NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard(notfication:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard(notfication:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
+        let hideTap = UITapGestureRecognizer.init(target: self, action: #selector(hideKeyboardTap(recognizer:)))
+        hideTap.numberOfTapsRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+    }
+    
+    func hideKeyboardTap(recognizer: UITapGestureRecognizer) -> Void {
+        self.view.endEditing(true)
     }
     
     func showKeyboard(notfication: Notification) -> Void {
         let rect = notfication.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
         keyboard = rect.cgRectValue
+        UIView.animate(withDuration: 0.4) { 
+            self.scrollView.frame.size.height = self.scrollViewHeight - self.keyboard.size.height
+        }
     }
+    
     func hideKeyboard(notfication: Notification) -> Void {
-        
+        UIView.animate(withDuration: 0.4) { 
+            self.scrollView.frame.size.height = self.view.frame.height
+        }
     }
     
     @IBAction func signUpBtn_clicked(_ sender: Any) {
